85a1281021e385cc154a398d4923473cfd121cd9
timo.schoemaker <timo.schoemaker@hotmail.de>
2022-08-08 20:40:18 +0200

    basic functionaliyt working

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,37 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
+import * as vscode from "vscode";
+import { GitAPI } from "./git_api";
+import { SimpleGitAPI } from "./simple-git-api";
+let git:GitAPI|null=null;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+    vscode.window.showInformationMessage(vscode.workspace.workspaceFolders![0]!.uri.fsPath);
+    git=new SimpleGitAPI(vscode.workspace.workspaceFolders![0]!.uri.fsPath);
+  let disposable = vscode.commands.registerCommand(
+    "code-browsing.git-prev",
+    async () => {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.git-prev', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+      // Display a message box to the user
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      await git!.backward();
+    }
+   
+  );
+  context.subscriptions.push(disposable);
+  disposable=vscode.commands.registerCommand("code-browsing.git-next",async ()=>{
+    await git!.forward();
+  });
 }
